697795e6fce3151824025ae67d556904b2484bd6
tsjmattick <jeremy@tethrasolutions.com>
2021-08-18 16:18:36 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Client from 'shopify-buy';
+import '../../shared/app.css';
+
+const client = Client.buildClient({
+  storefrontAccessToken: 'd42612ee49813638e9c399f31bb8c39a',
+  domain: 'consignmenthive.myshopify.com'
+});
+
+ReactDOM.render(
+  <App client={client}/>,
+  document.getElementById('root')
+);
